7f1dfb1fc88cf3a2e883d2242d5d5dd84dbe1779
Jayeshuttam <Jayeshuttam7844@gmail.com>
2021-02-16 17:04:14 -0500

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ResetPassword from './components/ResetPassword';
+import ChangePassword from './components/ChangePassword';
+
+import {Redirect, Route,BrowserRouter as Router,Switch, BrowserRouter} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/ResetPassword" component ={ResetPassword}/>
+      <Route path="/ChangePassword"component={ChangePassword}/>
+      <Redirect to="/ResetPassword"/>
+    </Switch>
+    </BrowserRouter>
+</>
+
+
+  );
+}
+
+export default App;
