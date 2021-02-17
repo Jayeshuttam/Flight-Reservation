30061104eb80ca0dd0bec85d3ede8cde1084e3c7
Jayeshuttam <38659267+Jayeshuttam@users.noreply.github.com>
2021-02-17 00:26:43 -0500

    Update mongo_db.js

--- backend/mongo_db.js
+++ backend/mongo_db.js
@@ -4,7 +4,7 @@
 const MongoClient = require('mongodb').MongoClient;
 function connect(){
 
-const uri = "mongodb+srv://jayesh:jayesh123@cluster0.xtvnk.mongodb.net/Fl?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jayesh:<password>@cluster0.xtvnk.mongodb.net/Fl?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   if(err)
